c5b8ce16859536191742b89d29fbd744692b77b5
Birajsitaula <birajsitaula7@gmail.com>
2025-02-14 22:53:06 +0545

    modified

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import express from "express";
 import "dotenv/config";
+import authRoutes from "../routes/authRoutes.js";
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use("/auth", authRoutes);
 
 const { PORT } = process.env;
 
